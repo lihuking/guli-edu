6df272f6c181bda11c364b5306976acfaaa7f4b0
李虎 <lihuking2021@aliyun.com>
2022-02-14 23:10:28 +0800

    更新五

---
+++ gmall/src/main/java/com/lihuking/gmall/mapper/UserInfroMapper.java
@@ -0,0 +1,10 @@
+package com.lihuking.gmall.mapper;
+
+
+import com.lihuking.gmall.entity.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserInfroMapper extends Mapper<UserInfo> {
+
+
+}
